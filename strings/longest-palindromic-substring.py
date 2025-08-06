b540cb41ae28b50c188eb890e381f0948481b678
Ezekiel Obanla <obanlaopeyemi1997@gmail.com>
2025-08-05 23:41:26 -1100

    Day 5: Solved Longest Palindromic Substring [Medium]

---
+++ strings/longest-palindromic-substring.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s):
+        def expandAroundCenter(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+        
+        longest = ""
+        for i in range(len(s)):
+            odd = expandAroundCenter(i, i)       # Odd length
+            even = expandAroundCenter(i, i + 1)  # Even length
+            
+            if len(odd) > len(longest):
+                longest = odd
+            if len(even) > len(longest):
+                longest = even
+        
+        return longest
