8165b7b7fa92dc2deeeeb4e963f137b233cb1bdf
Ezekiel Obanla <obanlaopeyemi1997@gmail.com>
2025-08-16 23:39:00 -1100

    Day 18: Solved the problem that involves finding the first and last position

---
+++ arrays/find-first-and-last-position.py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchRange(self, nums, target):
+        def findFirst(nums, target):
+            left, right = 0, len(nums) - 1
+            first = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] >= target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+                if nums[mid] == target:
+                    first = mid
+            return first
+
+        def findLast(nums, target):
+            left, right = 0, len(nums) - 1
+            last = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+                if nums[mid] == target:
+                    last = mid
+            return last
+
+        return [findFirst(nums, target), findLast(nums, target)]
