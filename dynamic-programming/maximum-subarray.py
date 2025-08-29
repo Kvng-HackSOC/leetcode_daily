a8aa9c61c55b6b1d02662242b83aea988474bb3f
Ezekiel Obanla <obanlaopeyemi1997@gmail.com>
2025-08-28 22:31:25 -1100

    solved a new problem

---
+++ dynamic-programming/maximum-subarray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSubArray(self, nums):
+        max_sum = nums[0]
+        curr_sum = nums[0]
+
+        for num in nums[1:]:
+            # Either extend the current subarray or start new at num
+            curr_sum = max(num, curr_sum + num)
+            max_sum = max(max_sum, curr_sum)
+
+        return max_sum
