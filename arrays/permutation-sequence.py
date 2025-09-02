38f1ff255a37863aaa46b06d0e6345e5b18c0de5
Ezekiel Obanla <obanlaopeyemi1997@gmail.com>
2025-09-01 23:49:39 -1100

    Solved the permutation sequence problem

---
+++ arrays/permutation-sequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        from math import factorial
+        
+        numbers = [str(i) for i in range(1, n + 1)]
+        k -= 1  # convert to 0-indexed
+        result = []
+
+        for i in range(n, 0, -1):
+            fact = factorial(i - 1)
+            index = k // fact
+            result.append(numbers[index])
+            numbers.pop(index)
+            k %= fact
+
+        return "".join(result)
